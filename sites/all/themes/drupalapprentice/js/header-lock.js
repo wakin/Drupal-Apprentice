33bd1e18ce9eae7d721e382ae9eafd4b3daad9b8
JRden 'Jake' Fadallan <jffadallan@gmail.com>
2014-02-01 10:06:06 +0800

    added drupal files

---
+++ sites/all/themes/drupalapprentice/js/header-lock.js
@@ -0,0 +1,8 @@
+(function ($) {
+     Drupal.behaviors.mainmenu = {
+          attach: function (context,settings) {
+             //Your jquery code here. In this example, it gives out Hello alert on site load
+             // alert("It's Working!");
+        }
+     }
+})(jQuery);
